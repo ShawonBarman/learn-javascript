79ff6ee6cc73f12f90830e34a451c6f37c60808f
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-03-30 19:48:16 +0600

    Add files via upload

---
+++ 1_printing.js
@@ -0,0 +1,36 @@
+/*
+What is JavaScript?
+=> JavaScript is a lightweight, interpreted programming language. It is designed for creating network-centric applications. It is complimentary to and integrated with Java. JavaScript is very easy to implement because it is integrated with HTML. It is open and cross-platform.
+*/
+
+/*
+In JavaScript, the console keyword refers to an object, a collection of data and actions, that we can use in our code. Keywords are words that are built into the JavaScript language, so the computer will recognize them and treats them specially.
+
+write console.log() what we put inside the parentheses will get printed, or logged, to the console.
+
+JavaScript can "display" data in different ways:
+    (1)Writing into an HTML element, using innerHTML.
+    (2)Writing into the HTML output using document.write().
+    (3)Writing into an alert box, using window.alert().
+    (4)Writing into the browser console, using console.log().
+*/
+
+
+console.log("Hello World!");
+
+alert("Please click ok");
+
+document.write("This is document write");
+
+
+/*
+var	  ----- Declares a variable
+let   -----	Declares a block variable
+const   -----	Declares a block constant
+if   -----	Marks a block of statements to be executed on a condition
+switch   -----	Marks a block of statements to be executed in different cases
+for   -----	Marks a block of statements to be executed in a loop
+function   -----	Declares a function
+return   -----	Exits a function
+try   -----	Implements error handling to a block of statements
+*/
