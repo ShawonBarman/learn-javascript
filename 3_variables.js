cc2c6aedbd49f5e541f1f2151d49182811f3d9c7
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-03-30 19:48:51 +0600

    Add files via upload

---
+++ 3_variables.js
@@ -0,0 +1,20 @@
+// what are variables? => Containers to store data values
+/*
+4 Ways to Declare a JavaScript Variable:
+    Using var
+    Using let
+    Using const
+    Using nothing
+*/
+
+var number1 = 24;
+var number2 = 19;
+console.log(number1 + number2);
+// Here number1 and number2 are the variables
+
+let firstName = 'Shawon';
+let lastName = 'Barman';
+console.log(firstName+' '+lastName);
+
+const Rate = 2.5;  //Rate = 1.2;
+console.log('Rate : '+Rate);
