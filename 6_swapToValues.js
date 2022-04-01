ab74d39371184c4059af7c38a9ce2e63bb7234ff
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-04-01 13:37:20 +0600

    Add files via upload

---
+++ 6_swapToValues.js
@@ -0,0 +1,9 @@
+let a = 'red';
+let b = 'blue';
+console.log('Before swaping : ');
+console.log('a : '+ a +'\n'+'b : '+ b);
+let temp = a;
+a = b;
+b = temp;
+console.log('After swaping : ');
+console.log('a : '+a+'\n'+'b : '+b);
