7af600d9b1507020f7aa7f504ddea5e7605df276
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-04-16 17:54:41 +0600

    Add files via upload

---
+++ 16_localstorage.js
@@ -0,0 +1,13 @@
+// The localStorage object allows you to save key/value pairs in the browser.
+
+// The localStorage object stores data with no expiration date.
+// The data is not deleted when the browser is closed, and are available for future sessions.
+
+localStorage.setItem('name', 'Shawon');
+
+console.log(localStorage);
+
+localStorage.removeItem('name');
+
+console.log(localStorage);
+// console.log(localStorage.getItem('name'));
